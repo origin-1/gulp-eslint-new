1ab239b080848d4f70fb1045750caf64a942f3cc
Francesco Trotta <>
2020-06-28 20:02:06 +0200

    update build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
 'use strict';
 
-const {src, task} = require('gulp');
-const eslint = require('.');
+const eslint                = require('.');
+const { series, src, task } = require('gulp');
 
-task('default', () => {
-	return src(['**/*.js', '!node_modules/**', '!coverage/**', '!test/fixtures/**'])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-});
+task
+(
+	'lint',
+	() =>
+		src(['*.js', 'example/**/*.js', 'test/**/*.js', '!test/fixtures/**'])
+			.pipe(eslint())
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError())
+);
+
+task
+(
+	'test',
+	callback => {
+		const { fork } = require('child_process');
+
+		const { resolve } = require;
+		const c8Path = resolve('c8/bin/c8');
+		const mochaPath = resolve('mocha/bin/mocha');
+		const childProcess
+		= fork
+		(c8Path, ['--reporter=html', '--reporter=text-summary', mochaPath, '--check-leaks']);
+		childProcess.on('exit', code => callback(code && 'Test failed'));
+	}
+);
+
+task('default', series('lint', 'test'));
