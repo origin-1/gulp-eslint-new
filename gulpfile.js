be88e58023f548e365844cc09c05b5647a2a42eb
Francesco Trotta <github@fasttime.org>
2021-05-02 23:27:34 +0200

    update dependencies and CI build

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ task
 (
 	'clean',
 	async() => {
-		const { promises: { rmdir } } = require('fs');
+		const { promises: { rm } } = require('fs');
 
-		await rmdir('coverage', { recursive: true });
+		const options = { force: true, recursive: true };
+		await rm('coverage', options);
 	}
 );
 
