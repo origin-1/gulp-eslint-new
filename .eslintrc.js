683fc33347ab9a84cabd5f8ac44bb710a89a2d72
Francesco Trotta <github@fasttime.org>
2022-11-07 02:14:17 +0100

    Update build and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,31 +8,33 @@ createConfig
     {
         files:      '*.js',
         jsVersion:  2020,
-        env:        { node: true },
-        extends:    'plugin:eslint-comments/recommended',
         rules:
         {
             'brace-style':      ['error', '1tbs'],
-            'curly':            ['error', 'all'],
             'indent':           'error',
-            'no-throw-literal': 'off',
-            'padded-blocks':    'off',
         },
     },
     {
         files:          '*.ts',
         tsVersion:      'latest',
-        extends:        'plugin:eslint-comments/recommended',
         parserOptions:  { project: 'tsconfig.json' },
         plugins:        ['tsdoc'],
         rules:
         {
             '@typescript-eslint/brace-style':   ['error', '1tbs'],
             '@typescript-eslint/indent':        'error',
-            'curly':                            ['error', 'all'],
-            'no-throw-literal':                 'off',
-            'padded-blocks':                    'off',
             'tsdoc/syntax':                     'error',
+        },
+    },
+    {
+        files:      ['*.js', '*.ts'],
+        env:        { node: true },
+        extends:    'plugin:eslint-comments/recommended',
+        rules:
+        {
+            'curly':            ['error', 'all'],
+            'no-throw-literal': 'off',
+            'padded-blocks':    'off',
         },
     },
 );
