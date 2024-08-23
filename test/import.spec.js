c4bdf0fc87b529e9a487dc87fbe9329462560356
Francesco Trotta <github@fasttime.org>
2024-08-23 18:44:45 +0200

    Fix for `gulp-load-plugins`

--- test/import.spec.js
+++ test/import.spec.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { strict: assert } = require('assert');
+const { strict: assert }    = require('assert');
+const { dirname }           = require('path');
+const { promisify }         = require('util');
 
 it
 (
@@ -21,5 +23,19 @@ it
             'results',
         ];
         assert.deepEqual(actual, expected);
+    },
+);
+
+// gulp-load-plugins uses the `resolve` package.
+it
+(
+    'lookup with resolve',
+    async () =>
+    {
+        const { default: resolve } = await import('resolve');
+        const packageIterator = () => [dirname(__dirname)];
+        const actual = await promisify(resolve)('gulp-eslint-new', { packageIterator });
+        const expected = require.resolve('gulp-eslint-new');
+        assert.strictEqual(actual, expected);
     },
 );
